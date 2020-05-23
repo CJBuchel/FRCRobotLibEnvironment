842075c73c8a4b1f0eeb6ef871d9275c0f35126c
CJBuchel <connorbuchel@gmail.com>
2020-05-23 21:37:06 +0800

    added cj-vision2.0

--- 4788/src/coprocessor/src/main/include/vision.h
+++ 4788/src/coprocessor/src/main/include/vision.h
@@ -1,20 +1,12 @@
 #pragma once
 
-#include "VisTrack.h"
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableEntry.h"
 #include "networktables/NetworkTableInstance.h"
-#include <thread>
 
 class curtin_frc_vision {
  public:
   void run();
  private:
-  CJ::VisionTracking vision;
-  cs::UsbCamera cam;
-
-  nt::NetworkTableEntry TargetX;
-  nt::NetworkTableEntry TargetY;
-  nt::NetworkTableEntry ImageHeight;
-  nt::NetworkTableEntry ImageWidth;
+  
 };
